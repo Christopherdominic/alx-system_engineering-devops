ef8188147dc93f9a2d79a65c6bbfb77c5f1213f6
christopher dominic <chriseze0@gmail.com>
2024-04-19 06:17:42 -0700

    hhhh

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Create a file
-file { '/tmp/holberton':
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  content => "I love Puppet\n",
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
 
